7e9317c3561d22bcf839911371e425b42c59b433
Jenny-high <okihomotmik@gmail.com>
2026-05-30 14:32:16 +0900

    fix: Google認証の不正な設定値を無効化

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,8 @@
 google_client_id = ENV["GOOGLE_CLIENT_ID"].presence || Rails.application.credentials.dig(:google_oauth, :client_id)
 google_client_secret = ENV["GOOGLE_CLIENT_SECRET"].presence || Rails.application.credentials.dig(:google_oauth, :client_secret)
-google_auth_configured = google_client_id.present? && google_client_secret.present?
+google_client_id_valid = google_client_id.to_s.match?(/\A[0-9A-Za-z_-]+\.apps\.googleusercontent\.com\z/)
+google_client_secret_valid = google_client_secret.present? && !google_client_secret.to_s.match?(/\A(?:dummy|test|your-|sample)/i)
+google_auth_configured = google_client_id_valid && google_client_secret_valid
 
 Rails.application.config.x.google_auth_configured = google_auth_configured
 
