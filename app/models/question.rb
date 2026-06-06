f00de7fd6bfdb84f028f14999bc343df43f0635b
kimmm <okihomotmik@gmail.com>
2026-06-06 16:04:58 +0900

    fix: サーベイ作成時の標準設問判定を修正 (#33)
    * fix: サーベイ作成時に標準設問を自動作成
    
    * fix: 標準設問の週表記を本番DBに合わせる

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,9 @@
 class Question < ApplicationRecord
   STANDARD_BODIES = [
-    "直近一週間に関して、あなたは自分がやりたいと思っている仕事ができていると思いますか？",
-    "直近一週間に関して、あなたは良いパフォーマンス（行動や成果）を発揮できたと思いますか？",
-    "直近一週間に関して、あなたは職場の人間関係が良好だったと思いますか？",
-    "直近一週間に関して、あなたは十分な睡眠を取れていますか？",
+    "直近1週間に関して、あなたは自分がやりたいと思っている仕事ができていると思いますか？",
+    "直近1週間に関して、あなたは良いパフォーマンス（行動や成果）を発揮できたと思いますか？",
+    "直近1週間に関して、あなたは職場の人間関係が良好だったと思いますか？",
+    "直近1週間に関して、あなたは十分な睡眠を取れていますか？",
     "直近1週間に関して、困ったことや壁にぶつかった際、上司に気兼ねなく相談・エスカレーションできる状態でしたか？"
   ].freeze
 
@@ -21,6 +21,12 @@ class Question < ApplicationRecord
 
   def self.standard_questions_ready?
     standard_ordered.size == STANDARD_BODIES.size
+  end
+
+  def self.ensure_standard_questions!
+    STANDARD_BODIES.each do |body|
+      find_or_create_by!(body: body)
+    end
   end
 
   private
